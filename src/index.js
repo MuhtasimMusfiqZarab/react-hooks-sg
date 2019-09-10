f045e21b4ab3fd60383fb088e1e7cc740f1cdeff
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2019-09-10 10:46:21 +0600

    initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// this is the first file to be rendered
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+//rendering to app
+ReactDOM.render(<App />, document.querySelector("#root"));
