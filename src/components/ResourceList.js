3d9fceca54daf9472c3e83999e305962cdab692b
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2019-09-10 12:50:04 +0600

    updates with useEffect

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,36 +1,30 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-class ResourceList extends Component {
-  state = {
-    resources: []
+const ResourceList = props => {
+  //initialize some state here
+  const [resources, setResources] = useState([]); //initialized as empty array
+
+  const fetchResource = async resource => {
+    const result = await axios.get(
+      `https://jsonplaceholder.typicode.com/${resource}`
+    );
+
+    setResources(result.data); // axios provides result inside of data
   };
 
-  //will only be called while 1st time mount (not the 2nd time even if refreshes)
-  async componentDidMount() {
-    const result = await axios.get(
-      `https://jsonplaceholder.typicode.com/${this.props.resource}`
-    );
-    this.setState({ resources: result.data }); // axios always gives the result indide of the data
-  }
+  //use effect will be called with a function which will run each time (componentDidMount() & componentDidUpdate())
+  // this will run everytime the component is rendered or updated
 
-  // called while re-rendering (for parent/ change in component level state)
-  async componentDidUpdate(prevProps) {
-    // to check difference with current prop & make request if there is a change
-    if (prevProps.resource !== this.props.resource) {
-      const result = await axios.get(
-        `https://jsonplaceholder.typicode.com/${this.props.resource}`
-      );
-      this.setState({ resources: result.data }); // axios always gives the result indide of the data
-    }
-  }
-  render() {
-    return (
-      <div>
-        <div>{this.state.resources.length}</div>
-      </div>
-    );
-  }
-}
+  useEffect(() => {
+    fetchResource(props.resource);
+  }, [props.resource]); //2nd argument checks if prop is changed & only then the function runs
+
+  return (
+    <div>
+      <div>{resources.length}</div>
+    </div>
+  );
+};
 
 export default ResourceList;
